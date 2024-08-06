6470b92da4eca942dd12392c776ae110ee1f9dc6
tamiross <tamiross.mbox@gmail.com>
2024-08-06 14:36:17 +0300

    base pallete

---
+++ src/pallete.ts
@@ -0,0 +1,20 @@
+/* 
+
+    This color pallete it taken from https://www.canva.com/colors/color-palettes/dark-metal/
+*/
+export const darkPallete = {
+  pewter: "#D6D6D6",
+  gray: "#8C8C8C",
+  ebony: "#44444C",
+  black: "#0B0909",
+};
+
+/* 
+    This color pallete it taken from https://www.canva.com/colors/color-palettes/window-tide/
+*/
+export const bluePallete = {
+  babyBlue: "#C3E0E5",
+  darkBlue: "#274472",
+  blueGrey: "#5885AF",
+  minightBlue: "#41729F",
+};
